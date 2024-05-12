49473025b55f20a63b0b147c6975c49d2ccd5d89
DonatoJoao <joaod.morais@gmail.com>
2024-05-12 14:31:38 -0300

    Organizando

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class Main {
+import javax.sound.sampled.SourceDataLine;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Type a word to search:");
+        String word = scn.next();
+
+        URI api = URI.create("https://api.dictionaryapi.dev/api/v2/entries/en/"+word);
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(api)
+                .build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        String json = response.body();
+        System.out.println(json);
+    }
+
 }
