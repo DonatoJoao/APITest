622f901b8316ba1430e72a6e7bd307558ac5fddd
DonatoJoao <joaod.morais@gmail.com>
2024-05-12 14:39:27 -0300

    Acrescentando comentário para minha próxima etada

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,19 +10,22 @@ public class Main {
     public static void main(String[] args) throws Exception {
 
 
-        Scanner scn = new Scanner(System.in);
+        Scanner scn = new Scanner(System.in); //para capturar entrada da palavra a ser pesquisada
         System.out.println("Type a word to search:");
         String word = scn.next();
 
         URI api = URI.create("https://api.dictionaryapi.dev/api/v2/entries/en/"+word);
-
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(api)
                 .build();
+
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
         String json = response.body();
         System.out.println(json);
     }
-
+        /*
+        TODO:
+            - Próximo passo é selecionar/modelar a saída com somente algumas informações desejadas
+         */
 }
